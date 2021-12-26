fb607ff8824579ac3cea59f8b1e43008d6ef5e9d
DuongPH <chieuduong1102@gmail.com>
2021-12-26 13:34:04 +0700

    Update BrandDTO

---
+++ Extensions/Extension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace eProject3_Vehicle_Showroom_Management.Extensions
+{
+    public class Extension
+    {
+        public Extension()
+        {
+        }
+
+        public static string ConvertToBase64(string imagepath)
+        {
+            using (Image image = Image.FromFile(imagepath))
+            {
+                using (MemoryStream m = new MemoryStream())
+                {
+                    image.Save(m, image.RawFormat);
+                    byte[] imageBytes = m.ToArray();
+
+                    // Convert byte[] to Base64 String
+                    string base64String = Convert.ToBase64String(imageBytes);
+                    return base64String;
+                }
+            }
+        }
+    }
+}
---
+++ Models/DTO/BrandDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace eProject3_Vehicle_Showroom_Management.Models.DTO
+{
+    public class BrandDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Missing Name of Brand")]
+        public string BrandName { get; set; }
+
+        public HttpPostedFileBase UrlLogo { get; set; }
+    }
+}
