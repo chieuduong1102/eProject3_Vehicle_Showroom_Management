9868fdbb3c7851cb57551af5eebabf64ff0c7d2b
rockle2000 <quanghhuuyy000@gmail.com>
2022-01-04 20:14:51 +0700

    them not

---
+++ Extensions/IsAdminAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+using System.Web.Routing;
+
+namespace eProject3_Vehicle_Showroom_Management.Extensions
+{
+    public class IsAdminAttribute : ActionFilterAttribute,IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(filterContext.HttpContext.Session["Admin"])))
+            {
+                filterContext.Result = new HttpUnauthorizedResult();
+            }
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            if (filterContext.Result == null || filterContext.Result is HttpUnauthorizedResult)
+            {
+                filterContext.Result = new RedirectToRouteResult(
+                new RouteValueDictionary
+                {
+                     { "controller", "LoginAdmin" },
+                     { "action", "Index" }
+                });
+            }
+        }
+    }
+}
